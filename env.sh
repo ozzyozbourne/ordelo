83f80417c60387056d278b0548552268d14850d9
ozzyozbourne <khanosaid726@gmail.com>
2025-03-26 02:14:29 -0400

    added api key settings

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export DB_URI="<enter key>"
+
+export SPOONACULAR_API_KEY="<enter key>"
+
+
+export HONEYCOMB_API_ENDPOINT="https://api.honeycomb.io:443" # US instance
+export OTEL_SERVICE_NAME="ordelo"
+export HONEYCOMB_API_KEY="<enter key>"
+
+export PORT="8080"
+
