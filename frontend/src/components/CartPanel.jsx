5ee67569b6b9c4c7f0b43d8490587550920a51aa
ahmedz17 <ahmedzoha12@gmail.com>
2025-05-09 03:25:56 -0400

    done

--- frontend/src/components/CartPanel.jsx
+++ frontend/src/components/CartPanel.jsx
@@ -1,10 +1,6 @@
 import { useShoppingContext } from "../context/ShoppingContext";
 import { useAuth } from "../context/AuthContext";
-<<<<<<< HEAD
-import { Link, useNavigate} from "react-router-dom";
-=======
 import { useNavigate } from "react-router-dom";
->>>>>>> f47642186b971fdd5378f97c308a519d8eb99e21
 
 function CartPanel() {
   const { 
